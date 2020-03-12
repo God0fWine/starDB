7ce226b9ce407f2895ebcca80697f5cf77b6a564
Denis Prorochuk <luciferwiches@gmail.com>
2020-03-12 13:22:11 +0200

    add random planet + api service became separeted

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (
+
+        <div className="loadingio-spinner-double-ring-uojrcnqq24r">
+            <div className="ldio-uap0du6rq7b">
+                <div></div>
+                <div></div>
+                <div><div></div></div>
+                <div><div></div></div>
+            </div>
+        </div>
+    );
+
+
+};
+
+export default Spinner;
+
+
+
