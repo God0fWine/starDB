6bdd5179661020156506c322222037e0d7fa044b
Denis Prorochuk <luciferwiches@gmail.com>
2020-03-13 15:30:29 +0200

    add loading

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -4,13 +4,14 @@ import './spinner.css';
 
 const Spinner = () => {
     return (
-
-        <div className="loadingio-spinner-double-ring-uojrcnqq24r">
-            <div className="ldio-uap0du6rq7b">
-                <div></div>
-                <div></div>
-                <div><div></div></div>
-                <div><div></div></div>
+        <div className="spinner">
+            <div className="loadingio-spinner-double-ring-uojrcnqq24r">
+                <div className="ldio-uap0du6rq7b">
+                    <div></div>
+                    <div></div>
+                    <div><div></div></div>
+                    <div><div></div></div>
+                </div>
             </div>
         </div>
     );
